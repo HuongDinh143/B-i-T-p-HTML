0ac389d6271e6cd25c6ce5b633ab6f10dbb5c5ef
huongdinh143 <dinhthuhuong143@gmail.com>
2024-07-31 21:42:47 +0900

    add file

---
+++ session08-js basic/baitap/bai3.js
@@ -0,0 +1,4 @@
+let a = + (prompt("Nhập vào chiều rộng"));
+let b = + (prompt("Nhập vào chiều dài"));
+let s = a*b;
+document.write("Diện tích hình chữ nhật bằng: ", s)
---
+++ session08-js basic/lythuyet/index.js
@@ -0,0 +1,25 @@
+document.write("rikkei academy!");
+/*
+- var, let, const
+- 3 ông này khác nhau như thế nào?
+*/
+// khai báo tuổi của 1 người
+// var age = 18; 
+// // age=20;
+// var age = 20;
+const age=20;
+var firstName;
+console.log("firstName" ,firstName);
+console.log("age: ", age);
+var fullName = "Vũ Hông Vân";
+let checkEmailLogin="";
+let $ = 7;
+let _ = 8;
+let class1 = "jv";
+let isBoolean = 5>4;
+console.log(isBoolean);
+console.log(fullName);
+console.log("Tên của bạn là: ", fullName);
+let checkIsNull = null;
+let checkIsNaN = "JS"-5;
+console.log(11111111,checkIsNaN);
